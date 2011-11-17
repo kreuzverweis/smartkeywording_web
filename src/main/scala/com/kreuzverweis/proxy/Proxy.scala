0bb0d13f55679d01064d66eecf98938f4e3d550f
Carsten Saathoff <carsten@kreuzverweis.com>
2011-11-17 18:43:30 +0100

    default case removed (was only for debugging)

--- src/main/scala/com/kreuzverweis/proxy/Proxy.scala
+++ src/main/scala/com/kreuzverweis/proxy/Proxy.scala
@@ -76,9 +76,6 @@ object Proxy extends Plan with ServerErrorResponse {
 	      case None => req.respond(Unauthorized ~> ResponseString("No credentials supplied."))
 	    }
 	  }
-	  case req @ _ => {
-	    req.respond(MethodNotAllowed ~> ResponseString("eek"))
-	  }
 	}
   
   def getToken(cookies: Map[String, Option[Cookie]]): Option[Token] = { 
