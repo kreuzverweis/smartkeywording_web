c41bef37f187688f91d03bd8a7745b14f24854fd
Carsten Saathoff <carsten@kreuzverweis.com>
2012-03-22 17:31:37 +0100

    credentials as command line params (temporarily), migrated to oauth2 api

--- src/main/scala/com/kreuzverweis/proxy/Server.scala
+++ src/main/scala/com/kreuzverweis/proxy/Server.scala
@@ -27,8 +27,11 @@ object Server {
   val http = new dispatch.nio.Http
 
   def main(args: Array[String]) {
-    val port = if (args.headOption.isDefined) args.head.toInt else 8888
-    val baseUri = if (args.drop(1).headOption.isDefined) Proxy.server = url(args.drop(1).head)
+    val client = args.headOption.get
+    val secret = args.drop(1).headOption.get
+    Proxy.client = (client, secret)
+    val port = if (args.drop(2).headOption.isDefined) args.head.toInt else 8888
+    if (args.drop(3).headOption.isDefined) Proxy.server = url(args.drop(1).head)
     unfiltered.jetty.Http(port)
       .filter(Proxy)
       .resources((new File("static")).toURI().toURL())
