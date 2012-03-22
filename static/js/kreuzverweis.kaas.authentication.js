2f3bebf66270b13dfab1a3c6a2c6c7f33846b283
Thomas Franz <thomas@kreuzverweis.com>
2012-03-22 15:03:42 +0100

    JavaScript of GUI modified to meet new OAuth2 api and proxy

--- static/js/kreuzverweis.kaas.authentication.js
+++ static/js/kreuzverweis.kaas.authentication.js
@@ -1,20 +1,65 @@
 /**
-   
-   Copyright 2012 Kreuzverweis Solutions GmbH
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+ Copyright 2012 Kreuzverweis Solutions GmbH
 
-       http://www.apache.org/licenses/LICENSE-2.0
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-         
-**/
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ **/
+
+function autoLogin(errorFunction, successFunction) {
+	var userid;
+	// if no token is available
+	if(!$.cookie('token')) {
+		// if no userid is available
+		if(!$.cookie('userid')) {
+			// create user and store userid in cookie
+			$.ajax({
+				url : "/users",
+				type : "POST",
+				data : '',
+				dataType : "xml",
+				error : function(jqXHR, textStatus, errorThrown) {
+					errorFunction.call(jqXHR);
+				},
+				success : function(xmlResponse) {
+					userid = $('user > id', xmlResponse).text();
+					$.cookie('userid', userid, {
+						expires : 365
+					});
+					console.log('userid is ' + $.cookie('userid'));
+					// try a request and check if it works
+				}
+			});
+		}
+		$.ajax({
+			url : "/users/" + userid + "/tokens",
+			type : "POST",
+			data : '',
+			dataType : "xml",
+			error : function(jqXHR, textStatus, errorThrown) {
+				errorFunction.call(jqXHR);
+			},
+			success : function(xmlResponse) {
+				var token = $('token > value', xmlResponse).text();
+				//var expires = $('token > expires', xmlResponse).text();
+				$.cookie('token', token);
+				console.log('token is ' + token);
+			}
+		});
+	}
+	// try a request and check if it works
+	login(errorFunction, successFunction);
+}
 
 function login(errorFunc, successFunc) {
 	// send credentials
@@ -27,43 +72,5 @@ function login(errorFunc, successFunc) {
 		success : function(xmlResponse) {
 			successFunc.call(xmlResponse);
 		}
-	});
-}
-
-function autoLogin(errorFunction,successFunction) {
-	// check for cookies
-	// if no cookies, request token/secret and set cookies
-	if(!$.cookie('token') || !$.cookie('secret')) {
-		// request token/secret from server
-		$.ajax({
-			url : "/credentials",
-			type : "POST",
-			data: '',
-			dataType : "xml",
-			error : function(jqXHR, textStatus, errorThrown) {
-				errorFunction.call(jqXHR);
-			},
-			success : function(xmlResponse) {
-				var token = $('token > id',xmlResponse).text();
-				var secret = $('token > secret',xmlResponse).text();
-				setCredentials(token, secret);
-				console.log('token is '+$.cookie('token')+' / secret is '+$.cookie('secret'));
-				// wait until cookie is set
-				// try a request and check if it works
-				login(errorFunction, successFunction);
-			}
-		});
-	} else {
-		login(errorFunction, successFunction);
-	}	
-}
-
-function setCredentials(token, secret) {
-	// set cookies
-	$.cookie('token', token, {
-		expires : 365
-	});
-	$.cookie('secret', secret, {
-		expires : 365
 	});
 }
